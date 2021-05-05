117dc81a422b605d17635324bd4d4cfc2e0f60a9
yayou <51052477+yayouu@users.noreply.github.com>
2021-05-05 13:42:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const buttonGenerator = document.querySelector("a")
 
 buttonGenerator.addEventListener("click", (e)=>{
     e.preventDefault()
-    fetch("http://aws.random.cat/meow")
+    fetch("https://aws.random.cat/meow")
         .then(response=>response.json())
         .then(data =>{
             boxDog.innerHTML = `<img src="${data.file}" alt="">`
